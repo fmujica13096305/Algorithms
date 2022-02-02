02ea6b42256eb0bdab0e696f84f5072f44402138
Fernando Mujica <fernando.mujica@sovos.com>
2022-02-01 23:56:29 -0300

    uyytuytu

--- src/company/InsertionSort.java
+++ src/company/InsertionSort.java
@@ -1,8 +1,47 @@
 package company;
 
+import java.util.Arrays;
+
 public class InsertionSort {
 
 
+    public static void main (String args[]){
 
+        int[] array = {};
+
+        int key = 0;
+        if (array.length > 0){
+             key = array[0];
+        }
+
+
+        System.out.println("aaa" + key);
+
+        for (int i = 1 ; i < array.length ; i++)
+        {
+
+            if ( key > array[i] ){
+                key = array[i];
+                insert(array , i -1 , key);
+            }
+        }
+
+        System.out.println(Arrays.toString(array));
+
+        // expected output : {-99, -1, 0, 1, 2, 3, 4}
+
+
+    }
+
+
+    static void insert(int[] array, int rightIndex, int value) {
+        for ( int i = rightIndex ; i >= 0 ; i --){
+            System.out.println(i);
+            if (array[i] >= value){
+                array[i+1] = array[i];
+                array[i] = value;
+            }
+        }
+    }
 
 }
