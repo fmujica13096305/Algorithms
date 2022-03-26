1c753547a31c87b9eeb8699f78137ed37e8eae4a
fmujica13096305 <fernandomujica2020@gmail.com>
2022-03-26 13:46:10 -0300

    fdsfsf

--- src/main/LPS.java
+++ src/main/LPS.java
@@ -3,31 +3,35 @@ package main;
 class LPS {
 
     public int findLPSLength(String st) {
-        return findLPSLengthRecursive(st, 0, st.length()-1);
+        return findLPSLengthRecursive(st, 0, st.length() - 1);
     }
 
     private int findLPSLengthRecursive(String st, int startIndex, int endIndex) {
-        if(startIndex > endIndex)
+        if (startIndex > endIndex)
             return 0;
 
-        // every sequence with one element is a palindrome of length 1
-        if(startIndex == endIndex)
+        // every string with one character is a palindrome
+        if (startIndex == endIndex)
             return 1;
 
         // case 1: elements at the beginning and the end are the same
-        if(st.charAt(startIndex) == st.charAt(endIndex))
-            return 2 + findLPSLengthRecursive(st, startIndex+1, endIndex-1);
+        if (st.charAt(startIndex) == st.charAt(endIndex)) {
+            int remainingLength = endIndex - startIndex - 1;
+            // check if the remaining string is also a palindrome
+            if (remainingLength == findLPSLengthRecursive(st, startIndex + 1, endIndex - 1))
 
-        // case 2: skip one element either from the beginning or the end
-        int c1 =  findLPSLengthRecursive(st, startIndex+1, endIndex);
-        int c2 =  findLPSLengthRecursive(st, startIndex, endIndex-1);
+              return remainingLength + 2;
+        }
+
+        // case 2: skip one character either from the beginning or the end
+        int c1 = findLPSLengthRecursive(st, startIndex + 1, endIndex);
+        int c2 = findLPSLengthRecursive(st, startIndex, endIndex - 1);
         return Math.max(c1, c2);
     }
 
     public static void main(String[] args) {
         LPS lps = new LPS();
-        System.out.println(lps.findLPSLength("abdbca"));
-        System.out.println(lps.findLPSLength("cddpd"));
-        System.out.println(lps.findLPSLength("pqr"));
+        System.out.println(lps.findLPSLength("hhhh"));
+
     }
 }
