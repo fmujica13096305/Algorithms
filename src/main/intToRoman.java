5831ddbddf519ffeed5782e6db730be8a3ad40fe
Fernando Mujica <fernando.mujica@sovos.com>
2022-03-28 21:50:44 -0300

    hgvhgfhftyrtyhgfhgfhfg

--- src/main/intToRoman.java
+++ src/main/intToRoman.java
@@ -8,21 +8,20 @@ public class intToRoman {
 
 
 
-        String s = intToRoman(54);
+        String s = intToRoman("1994");
         System.out.println(s);
 
     }
 
-    public static String intToRoman(int num) {
-        String res = "";
+    public static String intToRoman(String num) {
+
         LinkedHashMap<Integer,String> map = new LinkedHashMap<>();
+        map.put(1000,"M");
+        map.put(900,"CM");
         map.put(500,"D");
         map.put(400,"CD");
         map.put(100,"C");
         map.put(90,"XC");
-        map.put(1000,"M");
-        map.put(900,"CM");
-
         map.put(50,"L");
         map.put(40,"XL");
         map.put(10,"X");
@@ -31,16 +30,16 @@ public class intToRoman {
         map.put(4,"IV");
         map.put(1,"I");
 
-        while(num > 0){
-            for(int i : map.keySet()){
-                if(num >= i){
-                    res = res + map.get(i);
-                    num = num - i;
-                    break;
-                }
-            }
-        }
+        char arr[] = num.toCharArray();
 
+        int mult = 1;
+        String number = "";
+        String res = "";
+        for (int i = num.length() ; i > 0 ; i--){
+           number = arr[i - 1];
+           res = res + map.get(number * mult);
+           mult = mult * 10;
+        }
         return res;
     }
 }
